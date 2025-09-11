8fe3954ab623b7e9c3342be5771dff0f60816bb8
Sebastian <sebastian.kitschke@googlemail.com>
2025-09-11 16:52:51 +0200

    Add trustedproxy config for Nginx Proxy Manager / HTTPS setup

---
+++ config/trustedproxy.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Proxies
+    |--------------------------------------------------------------------------
+    |
+    | Hier kannst du die IP-Adressen deiner Proxy-Server eintragen.
+    | Mit "*" vertraust du allen Proxies (praktisch im Heimnetz oder
+    | beim Einsatz von Nginx Proxy Manager).
+    |
+    | Empfehlung: Für mehr Sicherheit später die konkrete IP deines
+    | NPM-Containers eintragen (z. B. "192.168.178.120").
+    |
+    */
+
+    'proxies' => '*',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Headers
+    |--------------------------------------------------------------------------
+    |
+    | Diese Header geben an, welche Informationen Laravel auswertet,
+    | wenn die App hinter einem Proxy läuft (Host, Schema, Port, etc.).
+    |
+    | Normalerweise reicht HEADER_X_FORWARDED_ALL für Setups mit
+    | Nginx Proxy Manager vollkommen aus.
+    |
+    */
+
+    'headers' => Request::HEADER_X_FORWARDED_ALL,
+
+];
