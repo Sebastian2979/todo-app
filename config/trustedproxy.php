28176d3744f13c6a4671a4d09f92b1649d69536e
Sebastian <sebastian.kitschke@googlemail.com>
2025-09-11 17:48:57 +0200

    Test Cokkies Route

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -20,7 +20,7 @@ return [
     | Welche Forwarded-Header Laravel auswertet. "ALL" deckt das Übliche ab.
     */
     'headers' =>
-    Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,17 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/probe', function () {
+    session(['probe' => now()->toDateTimeString()]);
+    return response()->json([
+        'secure' => request()->isSecure(),
+        'proto'  => request()->header('x-forwarded-proto'),
+        'host'   => request()->header('host'),
+        'port'   => request()->header('x-forwarded-port'),
+        'session' => session('probe'),
+    ]);
+});
+
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
